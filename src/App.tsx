5a7c60cb9b86ebbdcf92e152e57a362094c66dcb
João Marcos <barretojm@outlook.com>
2022-11-25 09:13:21 -0300

    create front

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Title } from './components/Title/Title';
-import Text from './components/Text/Text';
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
-  const nome = 'João Marcos';
   return(
     <>
-   <Title nome="João Marcos" />
-    <Text />
+    <Navbar />
     </>
   );
 }

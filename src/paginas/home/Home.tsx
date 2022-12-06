13230066c6444eeb2d253b5d244498fc2c18e074
João Marcos <barretojm@outlook.com>
2022-12-06 15:04:41 -0300

    add: functions

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import {Typography,  Grid, Button} from '@material-ui/core';
+import {Typography, Grid, Button} from '@material-ui/core';
 import {Box} from '@mui/material';
 import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
